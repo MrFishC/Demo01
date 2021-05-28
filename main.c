0b91ed5cb71c1dfc6d48b5d0dc41423a8486c177
jack <yuchengrun@pujiangkeji.net>
2021-05-28 15:25:05 +0800

    字符串

--- main.c
+++ main.c
@@ -1,10 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+void test07();
+
+//太过于细节，那么学习的内容就会很多，时间有限，以核心的为主
 
 //问题收集（练习就会了）
 //1.clion环境配置（乱码）
 //2.指针的基本用法
 //3.数组指针和指针数组
+
+//指针与一维二维数组的关系
+//1.定义二维数组，可以省略行的前提
+    //只有对全部元素赋初值时，那么第一维的长度才可以不给出，否则定义是错的
+
 void test01() {
     printf("指针第二次练习-一维数组与指针\n");
     printf("只要弄明白指针，其它的就很容易\n");
@@ -148,6 +158,8 @@ void test04() {
 //定义 int *p[n];
 //p+1时，则p指向下一个数组元素(这一点比较重要)
 //5.其它细节，见上方博客内容
+//笔记中有 对 数组指针和指针数组更简洁的 描述
+
 void test05() {
     printf("数组指针跟指针数组\n");
     int array[] = {1,2,3,4,5};
@@ -206,15 +218,238 @@ void test06() {
 
 }
 
+float aver(float a[],int n){
+    float sum = a[0],pingjunshu;
+    int i;
+    for(i=1;i<n;i++){
+        sum = sum + a[i];
+    }
+    pingjunshu=sum/n;
+    return pingjunshu;
+}
+
+float aver(float a[],int n);
+//？第2组的成绩在控制台未输完就限制了输入，原因不明
+void test07() {
+
+    float zu1[5],zu2[10];
+    int i;
+    printf("请输入第1组的学生成绩:\n");
+    for(i = 0;i<=4;i++){
+        scanf("%f",&zu1[i]);
+    }
+    printf("请输入第2组的学生成绩:\n");
+    for(i = 0;i<=9;i++){
+        scanf("%f",zu2[i]);
+    }
+
+    printf("第1组平均分是%f\n",aver(zu1,5));
+    printf("第2组平均分是%f\n",aver(zu2,10));
+}
+
+//指针与一维二维数组之间的关系
+void test08(){
+    int a[5] = {1,2,3,4,5};
+    int *ptr = (int *)(&a + 1);     //ptr是未知的，只知道地址的位置
+    printf("%d,%d",*(a + 1),*(ptr - 1));    //a[1] = 2，a[4] = 5
+}
+
+void test09(){
+    char *color[] = {"red","green","blue"};     //指针数组保存的是字符串地址
+    //读内容
+    //是否可以写？
+    //跟平台相关  结果可能会不同  变化的才是正确的
+    for(int i = 0;i < 3;i++){
+        puts(color[i]);
+        printf("color[%d]=%p\n",i,&color[i]);
+        color[i] = "color";
+        printf("color[%d]=%p\n",i,&color[i]);
+    }
+
+    //指针指向二维数组的行变量
+
+}
+
+//指针变量指向二维数组的行变量
+
+//指针的指针（二级指针）：http://c.biancheng.net/view/2016.html
+//二级指针的作用详解：https://blog.csdn.net/majianfei1023/article/details/46629065?utm_medium=distribute.pc_relevant.none-task-blog-2%7Edefault%7EBlogCommendFromMachineLearnPai2%7Edefault-9.control&depth_1-utm_source=distribute.pc_relevant.none-task-blog-2%7Edefault%7EBlogCommendFromMachineLearnPai2%7Edefault-9.control
+//用处：
+//eg：查找的函数
+//
+
+void test11(){
+    int i = 100;
+    int *pInt = &i;
+    int **ppInt = &pInt;       //指针的指针
+    printf("i       = %d\n",i);
+    printf("*pInt   = %d\n",*pInt);
+    printf("**ppInt = %d\n",**ppInt);
+}
+
+//https://blog.csdn.net/daimous/article/details/71436825
+//用变量a给出下面的定义
+//a) 一个整型数
+//int a;
+//b) 一个指向整型数的指针
+//int *a;
+//c) 一个指向指针的的指针，它指向的指针是指向一个整型数
+//int **a;
+//d) 一个有10个整型数的数组
+//int a[10];
+//e) 一个指向有10个整型数数组的指针
+//int (*a）[10];
+//f) 一个有10个指针的数组，该指针是指向一个整型数的
+//int *a[10];
+//g).指针函数
+//int *p（int i）:返回值为指针类型的函数;
+//h).函数指针
+//int（*p）（int i）:是指向一个返回值为int型的指针;
+
+//指针函数与函数指针:https://blog.csdn.net/ameyume/article/details/8220832
+//指针函数就是返回一个地址给调用者，用于需要地址的情况
+
+int max(int a,int b){
+    return a > b ? a : b;
+}
+
+//声明函数指针,这里的返回值是int类型，返回值可以是指针
+int (*f)(int,int);
+//函数指针的调用
+void test12() {
+    printf("使用函数指针：计算最大值\n");
+    f = max;
+    int c = (*f)(1,2);
+    printf("max = %d\n",c);
+}
+
+//函数的回调
+int call(int x,int y){
+    return x + y;
+}
+//第三个参数是函数指针
+int test13(int i1,int i2,int (*callback)(int,int)){
+    return callback(i1,i2);
+}
+
+void test14(int array[]){
+    printf("将数组名直接作为参数传递的情况:\n");
+    printf("sizeof = %d\n", sizeof(array));
+}
+
+//指针变量指向二维数组的行
+void test15(){
+    float fenshu[3][4] = {{65,67,70,60},{65,67,70,60},{65,67,70,60}};
+    float (*ptr)[4] = fenshu;   //指针指向二维数组的行
+    //遍历二维数组的每一个元素
+    for(int i = 0;i < 4;i++){
+        for(int j = 0;j < 3;j++){
+            //ptr + i：第i行
+            //*(ptr + i):第i行第一个元素
+            //*(ptr + i) + j:第i行第j个元素的地址
+            //*(*(ptr + i) + j):第i行第j个元素的值
+            printf("第%d行，第%d列，值为：%f\n",(i+1),(j+1),*(*(ptr + i) + j));
+        }
+    }
+}
+
+//字符串定义的三种方式
+//字符串 拼接    todo
+
+void test18(){
+    //C语言的字符串，就是字符数组
+    //C语言字符串用字符数组表示
+
+    printf("字符串的三种定义方式\n");
+
+    //特殊的字符数组(字符串)
+    char str[10] = {'h','e','l','l','o','\0'};
+
+    //'\0'可以省略
+    char str1[10] = "hello";
+
+    //指针str2是在栈中，"hello"是在常量池，该种方式定义的字符串只能读（同上面2种的区别）
+    char *str2 = "hello";
+
+    //打印查看效果
+    printf("str =%s\n",str);
+    printf("str1=%s\n",str1);
+    printf("str2=%s\n",str2);
+}
+
+void test19(){
+    //todo 输入一个字符串
+
+}
+
+void test20(){
+    //给字符数组赋值
+
+    char str[10] = "abc";
+
+//    int i;
+//    for(i= 0; i < 10; i++){
+//        str[i] = 'i';
+//    }
+
+    printf("str=%s\n",str);
+
+    printf("sizeof(str)=%d\n", sizeof(str));
+    printf("strlen(str)=%d\n", strlen(str));
+
+}
+
+//常量指针：指针的地址可以改变，但是指针指向的值不可以改变
+//指针常量：指针所指向的地址不可以改变
+
+//结构体
+//结构体初始化
+//结构体的访问
+//结构体对象定义成指针：
+    //1.指针 -> 成员变量
+    //2.(*指针).成员变量
+
+//共用体定义
+//共用体的访问
+//指针形式 同上
+//共用体和结构体长度的计算，使用sizeof就行(很少会让我们去手动计算其大小，如何人工计算[对齐]暂时不用去了解)
+union Data{
+    int i;
+    float f;
+    char str[20];
+};
+
+void test17(){
+    union Data data;
+
+}
+
 int main(){
-    test01();
+//    test01();
 //    test02();
 //    test03();
 //    test04();
 //    test05();
 //    test06();
+//    test07();
+//    test08();
+//    test09();
+//    test11();
+
+    //    test12();
+//    printf("result = %d\n",test13(10,20,call));
+
+//    int array[] = {1,2,3,4,5};
+//    test14(array);
+
+//    test15();
+//    test18();
+    test20();
+
     return 0;
 }
+
+
 
 
 
